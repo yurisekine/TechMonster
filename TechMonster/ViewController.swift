8c01e30a07fb137474173929c52b19420b667704
SEKINE YURI <yuri.1027ex@gmail.com>
2016-02-11 20:49:16 +0900

    Initial Commit

---
+++ TechMonster/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TechMonster
+//
+//  Created by SEKINE YURI on 2016/02/11.
+//  Copyright © 2016年 SEKINE YURI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
